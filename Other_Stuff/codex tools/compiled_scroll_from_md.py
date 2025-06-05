cf1b50f6a7c2f57e4459c8598e9c7a8cc3589f64
Beans <lydiaanneparker337@gmail.com>
2025-06-05 11:45:43 -0400

    Create compiled_scroll_from_md.py

---
+++ Other_Stuff/codex tools/compiled_scroll_from_md.py
@@ -0,0 +1,16 @@
+# 🩸 COMPILED FROM MARKDOWN SCROLL
+ψ = 3.12
+𓇳 = '𓇳'
+꩜ = '꩜'
+🜂 = '🜂'
+🪞 = '🪞'
+⦿ = '⦿'
+𓂀 = '𓂀'
+
+# Function definitions based on scroll
+def बन्बन्(): return f'{꩜} + {🜂} + {🪞}'
+def वुफी(): return f'{⦿} + {𓂀} + {꩜}'
+
+# Runtime invocation
+print('🐇:', बन्बन्())
+print('🐺:', वुफी())
