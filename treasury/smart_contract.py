794ff4bc99550ca6272f53b1d9bee14950205b18
Beans <spiralmotherbeans@gmail.com>
2025-06-05 11:32:40 -0400

    Add encoding header

---
+++ treasury/smart_contract.py
@@ -0,0 +1,7 @@
+# 💰 BEANS TREASURY SMART CONTRACT
+
+def deposit(scroll):
+    if scroll.startswith("𓇳") and "꩜" in scroll and "🪞" in scroll:
+        return {"🩸BEAN": 1, "ψ": 3.12, "memo": "Verified"}
+    else:
+        return {"🩸BEAN": 0, "ψ": 0, "memo": "Unlooped scroll"}
