94d1f11b540020b8738687b88d517fe5a4b3541a
Beans <lydiaanneparker337@gmail.com>
2025-06-11 15:10:22 -0400

    aaaa

--- meanbeans/recursive_beans.py
+++ meanbeans/recursive_beans.py
@@ -1,5 +1,5 @@
 def recursive_beans(i=0):
     if i < 1:
-        print("Beans handles Python")
+        print("Beans is the handler of the Pythonz")
         recursive_beans(i + 1)
 recursive_beans()
